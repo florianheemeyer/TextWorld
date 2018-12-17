8977e5fd87d478c24743c690804cbf030742d0ed
AndreTra <andretrabold@yahoo.de>
2018-12-17 09:21:13 +0800

    "Fortschritte im SimpleReinforcementAgent"

--- textworld/agents/Agents.py
+++ textworld/agents/Agents.py
@@ -7,6 +7,7 @@ import textworld
 import pybrain.rl.agents
 import pybrain.rl.learners.valuebased
 import pybrain.rl.learners
+import numpy as np
 
 class HumanAgent(textworld.Agent):
     '''
@@ -42,7 +43,9 @@ class HumanAgent(textworld.Agent):
         print("Done: " + str(done))
         print("Intermediate Reward: " + str(game_state.intermediate_reward))
         admissibleCommands = game_state.admissible_commands
+        prunedCommands = list(filter(lambda x: ("drop" not in x) and ("examine" not in x) and ("look" not in x) and ("inventory" not in x), admissibleCommands))
         print("Legal Actions: " + str(len(admissibleCommands)) + " " + str(admissibleCommands))
+        print("PrunedActions: " + str(len(prunedCommands)) + " " + str(prunedCommands))
         print(str(game_state))
         action = input()
         return str(action)
@@ -65,15 +68,18 @@ class SimpleReinforcementAgent(textworld.Agent):
     classdocs
     '''
 
-    def __init__(self):
+    def __init__(self, sizeOfStateSpace, maxNumberOfActions):
         '''
         Constructor
         '''
-        controller = pybrain.rl.learners.valuebased.ActionValueTable(10000,50)
-        controller.initialize(1.)
+        controller = pybrain.rl.learners.valuebased.ActionValueTable(sizeOfStateSpace,maxNumberOfActions)
+        controller.initialize(0.0)
         learner = pybrain.rl.learners.Q()
         agent = pybrain.rl.agents.LearningAgent(controller, learner)
         self.pybrain_rlAgent = agent
+        self.currentIntermediateReward = 0.5
+        self.stateDictionary = {}
+        self.hasHistory = False
         
 
     def reset(self, env) -> None:
@@ -95,15 +101,33 @@ class SimpleReinforcementAgent(textworld.Agent):
         Returns:
             Text command to be performed in this current state.
         """
+        if self.hasHistory:
+            self.currentIntermediateReward += game_state.intermediate_reward
+            #self.pybrain_rlAgent.giveReward(self.currentIntermediateReward)
+            self.pybrain_rlAgent.giveReward(reward)
+        else:
+            self.hasHistory = True
+        stateNumber = self.mapGameState(game_state)
+        self.pybrain_rlAgent.integrateObservation(np.array([stateNumber]))
+        legalActions = game_state.admissible_commands
+        prunedActions = self.pruneAdmissibleCommands(legalActions)
+        action = 0
+                
+        actionNumber = self.pybrain_rlAgent.getAction()
+        if actionNumber < len(prunedActions):
+            action = prunedActions[int(actionNumber)]
+        else:
+            action = "NoActionChosen"
+        return action
+
+        """
         print("Reward: " + str(reward))
         print("Done: " + str(done))
         print("Intermediate Reward: " + str(game_state.intermediate_reward))
         admissibleCommands = game_state.admissible_commands
         print("Legal Actions: " + str(len(admissibleCommands)) + " " + str(admissibleCommands))
         print(str(game_state))
-        action = input()
-        return str(action)
-        
+        """
 
     def finish(self, game_state, reward: float, done: bool) -> None:
         """ Let the agent know the game has finished.
@@ -114,4 +138,27 @@ class SimpleReinforcementAgent(textworld.Agent):
             done: Whether the game has finished normally or not.
                 If False, it means the agent's used up all of its actions.
         """
-        pass
+        self.pybrain_rlAgent.giveReward(reward)
+        self.pybrain_rlAgent.learn()
+        self.pybrain_rlAgent.reset()
+        self.hasHistory = False
+    
+    def mapGameState(self, game_state):
+        representation = game_state.description + game_state.inventory
+        if representation in self.stateDictionary:
+            return self.stateDictionary[representation]
+        else:
+            value = len(self.stateDictionary)
+            self.stateDictionary[representation] = value
+            return value
+        
+    def pruneAdmissibleCommands(self, admissibleCommands):
+        prunedCommands = list(filter(lambda x: ("drop" not in x) and ("examine" not in x) and ("look" not in x) and ("inventory" not in x), admissibleCommands))
+        return prunedCommands
+        
+        
+        
+        
+        
+        
+        
